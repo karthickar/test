976ca2b8172efd1c9d05140add34680228a1ddd6
karthickar <karthick.ar@gmail.com>
2017-05-23 17:54:07 +0400

    Modifying

--- killit.sh
+++ killit.sh
@@ -1,16 +1,13 @@
 #!/bin/bash -e
 
 AWS_REGION=""
-AWS_KEY_PATH=""
 AWS_KEY_PAIR_NAME=""
 AWS_ACCESS_KEY=""
 AWS_SECRET=""
-DB_USERNAME=""
-DB_PASSWORD=""
 
 APP="$1"
 ENV="$2"
 COUNT="$3"
 SIZE="$4"
 
-terraform destroy -var "access_key=${AWS_ACCESS_KEY}" -var "secret_key=${AWS_SECRET}" -var 'region=us-east-1' -var "aws_key_pair_path=${AWS_KEY_PATH}" -var "aws_key_pair_name=${AWS_KEY_PAIR_NAME}" -var "db_username=${DB_USERNAME}" -var "db_password=${DB_PASSWORD}" -var "app_name=${APP}" -var "environment=${ENV}" -var "instance_type=${SIZE}"
+terraform destroy -var "access_key=${AWS_ACCESS_KEY}" -var "secret_key=${AWS_SECRET}" -var 'region=us-east-1' -var "aws_key_pair_name=${AWS_KEY_PAIR_NAME}" -var "db_username=${DB_USERNAME}" -var "db_password=${DB_PASSWORD}" -var "app_name=${APP}" -var "environment=${ENV}" -var "instance_type=${SIZE}"
--- startit.sh
+++ startit.sh
@@ -3,13 +3,14 @@
 AWS_REGION=""
 AWS_ACCESS_KEY=""
 AWS_SECRET=""
+AWS_KEY_PAIR_NAME=""
 
 APP="$1"
 ENV="$2"
 COUNT="$3"
 SIZE="$4"
 
-terraform apply -var "access_key=${AWS_ACCESS_KEY}" -var "secret_key=${AWS_SECRET}" -var 'region=us-east-1' -var "app_name=${APP}" -var "environment=${ENV}" -var "instance_type=${SIZE}"
+terraform apply -var "access_key=${AWS_ACCESS_KEY}" -var "secret_key=${AWS_SECRET}" -var "aws_key_pair_name=${AWS_KEY_PAIR_NAME}" -var 'region=us-east-1' -var "app_name=${APP}" -var "environment=${ENV}" -var "instance_type=${SIZE}"
 export terraform_inventory=$(which terraform-inventory)
 export PUBLIC_ACCESS_POINT=$(terraform output | grep app_entrypoint_address | cut -d = -f2)
 export DB_URL=$(cat terraform.tfstate | grep endpoint | cut -d : -f2- | tr -d '"' | tr -d , | tr -d ' ')
